49316055bf21ffad80ae5b2f305adf347ec04e34
silvatiago <tiagoufcg.silva@gmail.com>
2013-11-08 16:49:57 -0300

    Lab1

--- lab1/Numeros/src/classes/SistemaNumerosPorExtenso.java
+++ lab1/Numeros/src/classes/SistemaNumerosPorExtenso.java
@@ -8,13 +8,17 @@ public class SistemaNumerosPorExtenso {
 		Extenso extenso = new Extenso();
 		String Numero = "";
 		System.out.print("Insira um número de 0 a 1000000000: ");
+		
 		while (Numero.equals("")) {
 			Scanner numero_entrada = new Scanner(System.in);
 			Numero = numero_entrada.nextLine();
+			
+			
 			try {
 				System.out.println("O Numero " + Numero + " por extenso é: "
 						+ extenso.numero_por_Extenso(Integer.parseInt(Numero)));
-			} catch (Exception e) {
+			} 
+			catch (Exception e) {
 				Numero = "";
 				System.out.print("Insira um numero de 0 a 1000000000: ");
 			}
