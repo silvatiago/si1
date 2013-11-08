3b525dd8cac412809768c57c938619ecfeb9476b
silvatiago <tiagoufcg.silva@gmail.com>
2013-11-08 00:46:37 -0300

    commit lab1

--- lab1/Numeros/src/classes/Extenso.java
+++ lab1/Numeros/src/classes/Extenso.java
@@ -16,13 +16,13 @@ public class Extenso {
             
             if (numero_passado >= 20 && numero_passado < 100){
             		String[] array_das_Dezenas = {"vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"};
-                    int num2 = numero_passado/10;
+                    int divisao = numero_passado/10;
                     int resto = numero_passado%10;
                     
                     if(resto == 0){
-                            return String.format("%s", array_das_Dezenas[num2-2]);
+                            return String.format("%s", array_das_Dezenas[divisao-2]);
                     }else{
-                            return String.format("%s e %s", array_das_Dezenas[num2-2], numero_por_Extenso(resto));
+                            return String.format("%s e %s", array_das_Dezenas[divisao-2], numero_por_Extenso(resto));
                     }
             }
 			return null;
