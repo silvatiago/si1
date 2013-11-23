caf35395ba2b273883a2ac5b6b21b8f469f890ff
silvatiago <tiagoufcg.silva@gmail.com>
2013-11-23 01:19:33 -0300

    Commit lab2

--- lab2/todolist/app/controllers/Application.java
+++ lab2/todolist/app/controllers/Application.java
@@ -7,8 +7,20 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok("Hello world");
-    }
+	public static Result index() {
+	    return  redirect(routes.Application.tasks());
+	  }
+	  
+	  public static Result tasks() {
+	    return TODO;
+	  }
+	  
+	  public static Result newTask() {
+	    return TODO;
+	  }
+	  
+	  public static Result deleteTask(Long id) {
+	    return TODO;
+	  }
 
 }
