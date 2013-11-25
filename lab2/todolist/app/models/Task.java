bd1da87c8cf482f947f93c3337fc9e40d83a1601
silvatiago <tiagoufcg.silva@gmail.com>
2013-11-25 14:09:58 -0300

    teste formulario

--- lab2/todolist/app/models/Task.java
+++ lab2/todolist/app/models/Task.java
@@ -2,9 +2,14 @@ package models;
 
 import java.util.*;
 
+import play.data.validation.Constraints.*;
+
 public class Task {
-    
+  
+	
   public Long id;
+  
+  @Required
   public String label;
   
   public static List<Task> all() {
